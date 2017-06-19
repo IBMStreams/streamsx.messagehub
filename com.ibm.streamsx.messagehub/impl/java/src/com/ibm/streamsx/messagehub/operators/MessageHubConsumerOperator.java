9ac76fcf359cd6e01a5ec7c030da04d8bfd1bbee
James Cancilla <cancilla@ca.ibm.com>
2017-06-19 07:36:14 -0400

    Add SPLDocs, rename parameter to messageHubCredentialsFile -- ref #5, closes #7

--- com.ibm.streamsx.messagehub/impl/java/src/com/ibm/streamsx/messagehub/operators/MessageHubConsumerOperator.java
+++ com.ibm.streamsx.messagehub/impl/java/src/com/ibm/streamsx/messagehub/operators/MessageHubConsumerOperator.java
@@ -23,7 +23,9 @@ public class MessageHubConsumerOperator extends AbstractKafkaConsumerOperator {
 
     private String messageHubCredsFile = MessageHubOperatorUtil.DEFAULT_MESSAGE_HUB_CREDS_FILE_PATH;
 
-    @Parameter(optional = true)
+    @Parameter(optional = true, name="messageHubCredentialsFile", description="Specifies the name of the file that contains "
+    		+ "the complete MessageHub credentials JSON. If not specified, this parameter will "
+    		+ "attempt to load the credentials from the file `etc/messagehub.json`.")
     public void setMessageHubCredsFile(String messageHubCredsFile) {
         this.messageHubCredsFile = messageHubCredsFile;
     }
@@ -42,5 +44,71 @@ public class MessageHubConsumerOperator extends AbstractKafkaConsumerOperator {
         super.loadFromAppConfig();
     }
     
-    public static final String DESC = "The MessageHubConsumer operator is used to read messages from the BlueMix MessageHub service.";
+    public static final String DESC = ""
+    		+ "The **MessageHubConsumer** operator is used to consume records from the Bluemix MessageHub service. "
+    		+ "The operator has been designed to make connectivity to the service as simple as possible. "
+    		+ "This is achieved in a number of different ways, from having default values for the **appConfigName** parameter "
+    		+ "to allowing the user to copy/paste the raw MessageHub Credentials JSON into either an application configuration "
+    		+ "property or a file.\\n"
+    		+ "\\n"
+    		+ "The following table lists the default values that have been set by this operator for a couple of parameters: \\n" + 
+    		"\\n" + 
+    		"---\\n" + 
+    		"| Parameter | Default Value | Description |\\n" + 
+    		"|===|\\n" + 
+    		"| **appConfigName** | `messagehub` | Users can choose to place the raw MesageHub Credentials JSON in a property called `messagehub.creds` in an application configuration called `messagehub`. The operator will extract the information needed to connect to MessageHub. |\\n" + 
+    		"|---|\\n" + 
+    		"| **messageHubCredentialsFile** | `etc/messagehub.json` | Users can paste the raw MessageHub Credentials JSON into a file pointed to by this parameter. The operator will extract the information needed to connect to MessageHub. By default, the operator will look for a file called `etc/messagehub.json`. |\\n" + 
+    		"---\\n" + 
+    		"\\n" +  
+    		"\\n" + 
+    		"# Setup\\n" + 
+    		"\\n" + 
+    		"This section outlines different options for enabling the **MessageHubConsumer** operator to connet to Bluemix MessageHub. "
+    		+ "Any of the following options can be used to configure the operator for connecting to Bluemix. \\n" + 
+    		"\\n" + 
+    		"**Save Credentials in Application Configuration Property**\\n" + 
+    		"\\n" + 
+    		"With this option, users can copy their MessageHub Credentials JSON from the MessageHub service and "
+    		+ "store it in an application configuration property called `messagehub.creds`. When the operator starts, "
+    		+ "it will look for that property and extract the information needed to connect. "
+    		+ "The following steps outline how this can be done: \\n" + 
+    		"\\n" + 
+    		" 1. Create an application configuration called `messagehub`.\\n" + 
+    		" 2. Create a property in the `messagehub` application configuration *named* `messagehub.creds`.\\n" + 
+    		"   * The *value* of the property should be the raw MessageHub Credentials JSON\\n" + 
+    		" 3. The operator will automatically look for an application configuration named `messagehub` and will extract "
+    		+ "the information needed to connect. Users only need to specify the topic(s) that they wish to consume messages "
+    		+ "from (set via the **topic** parameter).\\n" + 
+    		"\\n" + 
+    		"**NOTE:** Users can specify a different application configuration name by setting the **appConfigName** "
+    		+ "parameter. The operator will still look for a property called `messagehub.creds` containing the "
+    		+ "MessageHub Credentials JSON. \\n" + 
+    		"\\n" + 
+    		"\\n" + 
+    		"**Save Credentials in File**\\n" + 
+    		"\\n" + 
+    		"With this option, users can copy their MessageHub credentials JSON from the MessageHub service and store it in a "
+    		+ "file called `messagehub.json`. When the operator starts up it will read the credentials from that file and "
+    		+ "extract the information needed to connect. The following steps outline how this can be done: \\n" + 
+    		"\\n" + 
+    		" 1. Create a file called `messagehub.json` in the `<application_directory>/etc/` directory. \\n" + 
+    		" 2. Paste the MessageHub Credentials JSON into this file and save it. \\n" + 
+    		" 3. The operator will automatically look for the file `<application_directory>/etc/messagehub.json` and "
+    		+ "will extract the information needed to connect. Users only need to specify the topic(s) "
+    		+ "that they wish to consume messages from (set via the **topic** parameter).\\n" + 
+    		"\\n" + 
+    		"**NOTE:** Users can use the **messageHubCredentialsFile** parameter to specify a different file containing the MessageHub Credentials JSON.\\n" + 
+    		"\\n" + 
+    		"\\n" + 
+    		"# Additional Operator Details\\n" + 
+    		"\\n" + 
+    		"The MessageHub Toolkit wraps the `KafkaConsumer` and `KafkaProducer` operators from a specific version of the Kafka Toolkit. "
+    		+ "This implies that all of the functionality and restrictions provided by the Kafka Toolkit are inherited by the MessageHub Toolkit.\\n" + 
+    		"\\n" + 
+    		"This version of the MessageHub Toolkit wraps **Kafka Toolkit v1.x**. It is recommended that users review "
+    		+ "the Kafka Toolkit documentation for additional information on supported functionality. "
+    		+ "The Kafka Toolkit documentation can be found here: "
+    		+ "[ https://ibmstreams.github.io/streamsx.kafka/docs/v0.1.0.0/spldoc/html/index.html | Kafka Toolkit v1.x Documentation ].\\n" + 
+    		"\\n";
 }
