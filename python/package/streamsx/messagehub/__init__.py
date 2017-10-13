cb4bfa3fd05c37dace18fc0b9b15450c58290eba
Dan Debrunner <debrunne@us.ibm.com>
2017-10-13 14:57:29 -0700

    Initial Python package

---
+++ python/package/streamsx/messagehub/__init__.py
@@ -0,0 +1,1 @@
+from _messagehub import consumeAsJson, produceAsJson
---
+++ python/package/streamsx/messagehub/_messagehub.py
@@ -0,0 +1,85 @@
+from streamsx.topology.schema import CommonSchema
+
+def consumeAsJson(topology, topic, app_config_name=None, name=None):
+    _op = MessageHubConsumer(topology, schema=CommonSchema.Json, outputMessageAttributeName='jsonString', appConfigName=app_config_name, topic=topic)
+    return _op.stream
+
+def produceAsJson(stream, topic, app_config_name=None, name=None):
+    stream = stream.as_json()
+    _op = MessageHubProducer(stream, appConfigName=app_config_name, topic=topic)
+    _op.params('messageAttribute', _op.attribute(stream, 'jsonString'))
+    
+class MessageHubConsumer(streamsx.spl.op.Source):
+    def __init__(self, topology, schema, vmArg=None, appConfigName=None, clientId=None, messageHubCredentialsFile=None, outputKeyAttributeName=None, outputMessageAttributeName=None, outputTimestampAttributeName=None, outputOffsetAttributeName=None, outputPartitionAttributeName=None, outputTopicAttributeName=None, partition=None, propertiesFile=None, startPosition=None, startTime=None, topic=None, triggerCount=None, userLib=None, name=None):
+        kind="com.ibm.streamsx.messagehub::MessageHubConsumer"
+        inputs=None
+        schemas=schema
+        params = dict()
+        if vmArg is not None:
+            params['vmArg'] = vmArg
+        if appConfigName is not None:
+            params['appConfigName'] = appConfigName
+        if clientId is not None:
+            params['clientId'] = clientId
+        if messageHubCredentialsFile is not None:
+            params['messageHubCredentialsFile'] = messageHubCredentialsFile
+        if outputKeyAttributeName is not None:
+            params['outputKeyAttributeName'] = outputKeyAttributeName
+        if outputMessageAttributeName is not None:
+            params['outputMessageAttributeName'] = outputMessageAttributeName
+        if outputTimestampAttributeName is not None:
+            params['outputTimestampAttributeName'] = outputTimestampAttributeName
+        if outputOffsetAttributeName is not None:
+            params['outputOffsetAttributeName'] = outputOffsetAttributeName
+        if outputPartitionAttributeName is not None:
+            params['outputPartitionAttributeName'] = outputPartitionAttributeName
+        if outputTopicAttributeName is not None:
+            params['outputTopicAttributeName'] = outputTopicAttributeName
+        if partition is not None:
+            params['partition'] = partition
+        if propertiesFile is not None:
+            params['propertiesFile'] = propertiesFile
+        if startPosition is not None:
+            params['startPosition'] = startPosition
+        if startTime is not None:
+            params['startTime'] = startTime
+        if topic is not None:
+            params['topic'] = topic
+        if triggerCount is not None:
+            params['triggerCount'] = triggerCount
+        if userLib is not None:
+            params['userLib'] = userLib
+        super(MessageHubConsumer, self).__init__(topology,kind,schemas,params,name)
+
+
+
+class MessageHubProducer(streamsx.spl.op.Sink):
+    def __init__(self, stream, vmArg=None, appConfigName=None, keyAttribute=None, messageAttribute=None, messageHubCredentialsFile=None, partitionAttribute=None, propertiesFile=None, timestampAttribute=None, topicAttribute=None, topic=None, userLib=None, name=None):
+        topology = stream.topology
+        kind="com.ibm.streamsx.messagehub::MessageHubProducer"
+        inputs=stream
+        schemas=None
+        params = dict()
+        if vmArg is not None:
+            params['vmArg'] = vmArg
+        if appConfigName is not None:
+            params['appConfigName'] = appConfigName
+        if keyAttribute is not None:
+            params['keyAttribute'] = keyAttribute
+        if messageAttribute is not None:
+            params['messageAttribute'] = messageAttribute
+        if messageHubCredentialsFile is not None:
+            params['messageHubCredentialsFile'] = messageHubCredentialsFile
+        if partitionAttribute is not None:
+            params['partitionAttribute'] = partitionAttribute
+        if propertiesFile is not None:
+            params['propertiesFile'] = propertiesFile
+        if timestampAttribute is not None:
+            params['timestampAttribute'] = timestampAttribute
+        if topicAttribute is not None:
+            params['topicAttribute'] = topicAttribute
+        if topic is not None:
+            params['topic'] = topic
+        if userLib is not None:
+            params['userLib'] = userLib
+        super(MessageHubProducer, self).__init__(topology,kind,inputs,params,name)
