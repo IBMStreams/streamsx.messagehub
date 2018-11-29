85cb0eb2253f89bfcd56a96f12f5ec407f243f1d
RolefH <rolef.heinrich@de.ibm.com>
2018-11-29 16:30:37 +0100

    toolkit version bumped to 1.6.0 with kafka base version v1.6.0.1; SPLDoc

--- com.ibm.streamsx.messagehub/impl/java/src/com/ibm/streamsx/messagehub/operators/MessageHubProducerOperator.java
+++ com.ibm.streamsx.messagehub/impl/java/src/com/ibm/streamsx/messagehub/operators/MessageHubProducerOperator.java
@@ -141,6 +141,8 @@ public class MessageHubProducerOperator extends AbstractKafkaProducerOperator {
             + "\\n"
             + KafkaSplDoc.PRODUCER_AUTOMATIC_SERIALIZATION
             + "\\n"
+            + KafkaSplDoc.PRODUCER_EXPOSED_KAFKA_METRICS
+            + "\\n"
             + KafkaSplDoc.CHECKPOINTING_CONFIG
             + "\\n"
             + KafkaSplDoc.PRODUCER_CONSISTENT_REGION_SUPPORT
